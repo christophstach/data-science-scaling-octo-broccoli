66238fd4e1a238c84b312068529238b783d441a4
Christoph Stach <christoph.stach@gmail.com>
2017-04-27 14:59:58 +0200

    Added some code. Is not working at the moment

---
+++ src/main/java/edu/stachsaizhartgers/broccoli/config/ApiConfig.java
@@ -0,0 +1,10 @@
+package edu.stachsaizhartgers.broccoli.config;
+
+/**
+ * Created by Christoph Stach on 4/27/17.
+ *
+ * Apis config
+ */
+public class ApisConfig {
+  private TwitterConfig twitter;
+}
---
+++ src/main/java/edu/stachsaizhartgers/broccoli/config/TwitterConfig.java
@@ -0,0 +1,7 @@
+package edu.stachsaizhartgers.broccoli.config;
+
+/**
+ * Created by Christoph Stach on 4/27/17.
+ */
+public class TwitterConfig {
+}
