a165eb116bb4aeefa13fe0c53729f1c1efdc64c4
Christoph Stach <christoph.stach@gmail.com>
2017-05-18 11:04:37 +0200

    improved data-miner

--- twitter-data-miner.sh
+++ twitter-data-miner.sh
@@ -1,15 +1,21 @@
 #!/bin/sh
 # This file runs the jar file in the background and after a user logged out from the shell.
 # A PID file will be created to determine if the program is already running.
-SERVICE_NAME=TwitterClientService
-PATH_TO_JAR=/home/christoph/Projects/data-science-scaling-octo-broccoli/target/data-science-scaling-octo-broccoli-1.0-SNAPSHOT-jar-with-dependencies.jar
-PID_PATH_NAME=/tmp/twitter-client-service-pid
+SERVICE_NAME=TwitterDataMiner
+PATH_TO_JAR=./data-science-scaling-octo-broccoli-1.0.0-jar-with-dependencies.jar
+
+# PID_PATH_NAME=/tmp/twitter-data-miner-german-stop-words-pid
+# PID_PATH_NAME=/tmp/twitter-data-miner-germany-geo-pid
+# PID_PATH_NAME=/tmp/twitter-data-miner-berlin-geo-pid
+
+# CONFIG_FILE=./config.german-stop-words.yml
+# CONFIG_FILE=./config.germany-geo.yml
+# CONFIG_FILE=./config.berlin-geo.yml
 case $1 in
     start)
         echo "Starting $SERVICE_NAME ..."
         if [ ! -f $PID_PATH_NAME ]; then
-            nohup java -jar $PATH_TO_JAR /tmp 2>> /dev/null >> /dev/null &
-                        echo $! > $PID_PATH_NAME
+            nohup java -jar $PATH_TO_JAR $CONFIG_FILE & echo $! > $PID_PATH_NAME
             echo "$SERVICE_NAME started ..."
         else
             echo "$SERVICE_NAME is already running ..."
